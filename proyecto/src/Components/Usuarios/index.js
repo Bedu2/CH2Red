d1d40927747ef3498c3cd579a8702d9ab0aac9d2
Martín Pereyra-Gamboa <41092320+pereyragamboa@users.noreply.github.com>
2018-09-26 22:49:56 -0500

    Corrección en mostrarUsuarios()

--- proyecto/src/Components/Usuarios/index.js
+++ proyecto/src/Components/Usuarios/index.js
@@ -18,33 +18,44 @@ class Usuarios extends Component {
       </div>
     );
 
-  mostrarUsuarios = () => {
-    this.props.usuarios.map((usuario) => (
-      <Table>
-        <thead>
-        <tr>
-          <th className="hide-on-med-and-up">Nombre completo</th>
-          <th className="hide-on-small-only">Nombre</th>
-          <th className="hide-on-small-only">Apellido Paterno</th>
-          <th className="hide-on-small-only">Apellido Materno</th>
-          <th className="hide-on-small-only">Edad</th>
-        </tr>
-        </thead>
-        <tbody>
+  mostrarUsuarios = () => (
+    <Table>
+      <thead>
+      <tr>
+        <th className="hide-on-med-and-up">Nombre completo</th>
+        <th className="hide-on-small-only">Nombre</th>
+        <th className="hide-on-small-only">Apellido Paterno</th>
+        <th className="hide-on-small-only">Apellido Materno</th>
+        <th className="hide-on-small-only">Edad</th>
+      </tr>
+      </thead>
+      <tbody>
+      { this.props.usuarios.map((usuario) => (
           <tr>
             <td className="hide-on-med-and-up">{this.obtenerNombreCompleto(usuario)}</td>
             <td className="hide-on-small-only">{usuario.nombre}</td>
             <td className="hide-on-small-only">{usuario.apellidos.paterno}</td>
-            <td className="hide-on-small-only">{usuario.apellidos.materno}Gomez</td>
+            <td className="hide-on-small-only">{usuario.apellidos.materno}</td>
             <td className="hide-on-small-only">{usuario.edad}</td>
-            <td><Link to={`/verUsuario/${usuario.id}`}><Icon>visibility</Icon></Link></td>
-            <td><Link to={`/editarUsuario/${usuario.id}`}><Icon>edit</Icon></Link></td>
-            <td><Link to={`/eliminarUsuario/${usuario.id}`}><Icon>delete_forever</Icon></Link></td>
-          </tr>
-        </tbody>
-      </Table>
-    ));
-  };
+            <td><Link to={`/verUsuario/${usuario.id}`}><Button icon="visibility"/></Link></td>
+            <td><Link to={`/editarUsuario/${usuario.id}`}><Button icon="edit"/></Link></td>
+            <td>
+              <Modal header="Eliminar usuario">
+                actions={
+                <div>
+                  <Button className="red modal-close">Sí</Button>&nbsp;
+                  <Button className="green modal-close">No</Button>
+                </div>}
+                trigger={<Button icon="delete_forever"/>}>
+                <p>¿Desea eliminar a {usuario.nombre}?</p>
+              </Modal>
+            </td>
+            <td><Link to={`/eliminarUsuario/${usuario.id}`}><Button icon="delete_forever"/></Link></td>
+          </tr> ))
+      }
+      </tbody>
+    </Table>
+  );
 
   obtenerNombreCompleto = (usuario) =>
     `${usuario.nombre} ${usuario.apellidos.paterno} ${usuario.apellidos.materno}`;
