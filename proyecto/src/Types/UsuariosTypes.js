218fd636598e7e13164a1c36fd5ea7f5837265bc
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-10-03 17:26:41 -0500

    Actualización de UsuariosTypes

--- proyecto/src/Types/UsuariosTypes.js
+++ proyecto/src/Types/UsuariosTypes.js
@@ -1,8 +1,11 @@
-export const CAMBIAR_USUARIO_NOMBRE = 'cambiarUsuarioNombre';
-export const CAMBIAR_USUARIO_APELLIDO_PATERNO = 'cambiarUsuarioApellidoPaterno';
-export const CAMBIAR_USUARIO_APELLIDO_MATERNO = 'cambiarUsuarioApellidoMaterno';
-export const CAMBIAR_USUARIO_EDAD = 'cambiarUsuarioEdad';
-export const ERROR = 'error';
-export const INICIANDO = 'iniciando';
-export const USUARIOS_CARGADOS = 'usuariosCargados';
-export const CONSULTA_USUARIOS = 'consultaUsuarios';
+		
+export const CAMBIO_NOMBRE= 'cambiarUsuarioNombre';
+export const CAMBIO_APELLIDO_PATERNO= 'cambiarUsuarioApellidoPaterno';
+export const CAMBIO_APELLIDO_MATERNO= 'cambiarUsuarioApellidoMaterno';
+export const CAMBIO_EDAD= 'cambiarUsuarioEdad';
+export const ERROR_USUARIOS= 'error';
+export const EMPEZAR_USUARIOS= 'iniciando';
+export const CARGANDO_USURIOS= 'usuariosCargados';
+export const CONSULTA_USUARIOS= 'consultaUsuarios';
+export const USUARIO_SOLO_LECTURA = 'usuarioSoloLectura';
+export const USUARIO_DATOS_COMPLETOS = 'usuarioDatosCompletos';
