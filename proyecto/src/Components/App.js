a03e0cf3b8eec06c6f83dd2a09ef4036c5a8a9da
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-10-13 17:24:25 -0500

    Ajuste de tabulaciones + archivos superfluos eliminados
    Los archivos eliminados fueron algunos creados con `create-react-app` que no se usan en el proyecto.

--- proyecto/src/Components/App.js
+++ proyecto/src/Components/App.js
@@ -5,30 +5,29 @@ import Usuarios from './Usuarios';
 import AgregarEditar from './Usuarios/AgregarEditarUsuarios';
 import Dependientes from './Dependientes';
 import AgregarEditarDependiente from './Dependientes/AgregarEditarDependientes';
-import '../App.css';
 import * as Rutas from '../Paths';
 
 const App = () => (
   <div >
-      	<BrowserRouter>
-      		<div>
-      			<Header />
-      			<br/>
-      			<br/>
-      			<div className='container'  >
-      				<Route exact path='/' component= {Usuarios} />
-      				<Route exact path={Rutas.RUTA_AGREGAR_USUARIO} component= {AgregarEditar} />
-      				<Route exact path={`${Rutas.RUTA_EDITAR_USUARIO}:id`} component= {AgregarEditar} />
-      				<Route exact path={`${Rutas.RUTA_LISTA_DEPENDIENTES}:id`} component= {Dependientes} />
-      				<Route exact path={`${Rutas.RUTA_AGREGAR_DEPENDIENTE}:usuarioId`}
-										 component= {AgregarEditarDependiente} />
-      				<Route exact path={`${Rutas.RUTA_EDITAR_DEPENDIENTE}:dependienteId`}
-										 component= {AgregarEditarDependiente} />
-      			</div>
-      		</div>
-
-        </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <br/>
+        <br/>
+        <div className='container'  >
+          <Route exact path='/' component= {Usuarios} />
+          <Route exact path={Rutas.RUTA_AGREGAR_USUARIO} component= {AgregarEditar} />
+          <Route exact path={`${Rutas.RUTA_EDITAR_USUARIO}:id`} component= {AgregarEditar} />
+          <Route exact path={`${Rutas.RUTA_LISTA_DEPENDIENTES}:id`} component= {Dependientes} />
+          <Route exact path={`${Rutas.RUTA_AGREGAR_DEPENDIENTE}:usuarioId`}
+                 component= {AgregarEditarDependiente} />
+          <Route exact path={`${Rutas.RUTA_EDITAR_DEPENDIENTE}:dependienteId`}
+                 component= {AgregarEditarDependiente} />
+        </div>
       </div>
+
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
--- proyecto/src/Components/Dependientes/AgregarEditarDependientes.js
+++ proyecto/src/Components/Dependientes/AgregarEditarDependientes.js
@@ -6,36 +6,36 @@ import * as ActionsDependientes from '../../Actions/DependientesActions';
 import { RUTA_LISTA_DEPENDIENTES } from "../../Paths";
 
 class AgregarEditarDependiente extends Component {
-	componentDidMount() {
-		console.log(this.props.match.params);
-		if (this.props.match.params.dependienteId) {
-		  // `dependienteId` se define en /editarDependiente/
-			this.props.obtenerDependiente(this.props.match.params.dependienteId);
-		}
-		else if (this.props.match.params.usuarioId) {
-		  // `usuarioId` se define en /agregarDependiente/
-			this.props.asignarIdUsuario(this.props.match.params.usuarioId);
-			if (this.props.idDependiente) {
-			  // Si `idDependiente` tiene un valor asignado, hay una edición
+  componentDidMount() {
+    console.log(this.props.match.params);
+    if (this.props.match.params.dependienteId) {
+      // `dependienteId` se define en /editarDependiente/
+      this.props.obtenerDependiente(this.props.match.params.dependienteId);
+    }
+    else if (this.props.match.params.usuarioId) {
+      // `usuarioId` se define en /agregarDependiente/
+      this.props.asignarIdUsuario(this.props.match.params.usuarioId);
+      if (this.props.idDependiente) {
+        // Si `idDependiente` tiene un valor asignado, hay una edición
         // de dependiente en curso; pero al haber entrado al modo
         // de agregar nuevo dependiente, se descartan los datos ya
         // existentes
         this.props.limpiarFormularioDependientes();
       }
-		}
-	}
+    }
+  }
 
-	validarFormularioDependiente = () => (
-		this.props.nombre_completo && this.props.edad && this.props.dependencia
-	);
+  validarFormularioDependiente = () => (
+    this.props.nombre_completo && this.props.edad && this.props.dependencia
+  );
 
-	crearNuevoDependiente = () => {
-		return {
-			nombre_completo: this.props.nombre_completo,
-			dependencia: this.props.dependencia,
-			edad: this.props.edad,
-			_usuario: this.props._usuario
-	}};
+  crearNuevoDependiente = () => {
+    return {
+      nombre_completo: this.props.nombre_completo,
+      dependencia: this.props.dependencia,
+      edad: this.props.edad,
+      _usuario: this.props._usuario
+  }};
 
   cambiarNombreCompleto = (event) => {
     this.props.cambiarNombreCompleto(event.target.value);
@@ -49,49 +49,49 @@ class AgregarEditarDependiente extends Component {
     this.props.cambiarEdad(event.target.value)
   };
 
-	mostrarPreloader = () => (
-		<div className="center-align">
-			<Preloader/>
-		</div>
-	);
-
-	mostrarBotonGuardarDependientes = () => (
-    <div className="row">
-			<Link to={`${RUTA_LISTA_DEPENDIENTES}${this.props._usuario}`}>
-				<Button  className="col s6 offset-m2 m4" waves='light'
-								disabled= { !this.validarFormularioDependiente() }
-								onClick={ this.clickGuardarDependiente }>
-					Guardar
-				</Button>
-			</Link>
-			<Button className="col s6 m4 red" waves='light'
-							onClick={ () => this.props.activarRedireccion(true) }>
-				Cancelar
-			</Button>
+  mostrarPreloader = () => (
+    <div className="center-align">
+      <Preloader/>
     </div>
   );
 
-	clickGuardarDependiente = event => {
-		const datosDependiente = this.crearNuevoDependiente();
-		const id = this.props.match.params.dependienteId;
-		id ?
-			this.props.modificarDependiente(id, datosDependiente) :
-			this.props.agregarDependiente(datosDependiente);
-	};
+  mostrarBotonGuardarDependientes = () => (
+    <div className="row">
+      <Link to={`${RUTA_LISTA_DEPENDIENTES}${this.props._usuario}`}>
+        <Button  className="col s6 offset-m2 m4" waves='light'
+                disabled= { !this.validarFormularioDependiente() }
+                onClick={ this.clickGuardarDependiente }>
+          Guardar
+        </Button>
+      </Link>
+      <Button className="col s6 m4 red" waves='light'
+              onClick={ () => this.props.activarRedireccion(true) }>
+        Cancelar
+      </Button>
+    </div>
+  );
 
-	render() {
-		return ( this.props.redireccionar ? ( <Redirect to= {`${RUTA_LISTA_DEPENDIENTES}${this.props._usuario}`} />):
-			(<div className="row">
+  clickGuardarDependiente = event => {
+    const datosDependiente = this.crearNuevoDependiente();
+    const id = this.props.match.params.dependienteId;
+    id ?
+      this.props.modificarDependiente(id, datosDependiente) :
+      this.props.agregarDependiente(datosDependiente);
+  };
+
+  render() {
+    return ( this.props.redireccionar ? ( <Redirect to= {`${RUTA_LISTA_DEPENDIENTES}${this.props._usuario}`} />):
+      (<div className="row">
         <Input s={12} label="Nombre" type='text' placeholder=' '
                onChange={this.cambiarNombreCompleto} value={this.props.nombre_completo} />
         <Input s={12} m={8} label="Dependencia" type='text' placeholder=' '
                onChange={this.cambiarDependencia} value={ this.props.dependencia } />
         <Input s={12} m={4} label="Edad" type='text' placeholder=' '
                onChange={this.cambiarEdad} value={ this.props.edad } />
-				{ this.props.cargando ? this.mostrarPreloader() : this.mostrarBotonGuardarDependientes() }
-			</div>)
-		);
-	}
+        { this.props.cargando ? this.mostrarPreloader() : this.mostrarBotonGuardarDependientes() }
+      </div>)
+    );
+  }
 }
 
 const mapStateToProps = ({ DependientesReducer }) => DependientesReducer;
